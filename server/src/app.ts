0c9f3df0be5fa60f48ba3584f3cbc8b7a0fa39e6
asijain019 <asi82005.j@gmail.com>
2026-05-10 18:41:23 +0530

    Fix Express 5 wildcard routing syntax

--- server/src/app.ts
+++ server/src/app.ts
@@ -42,7 +42,7 @@ const distPath = path.resolve(__dirname, "../../client/dist");
 app.use(express.static(distPath));
 
 // Handle React routing, return all requests to React app
-app.get("*", (req, res) => {
+app.get("(.*)", (req, res) => {
   res.sendFile(path.resolve(distPath, "index.html"));
 });
 
